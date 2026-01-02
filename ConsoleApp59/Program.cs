66132ca148dfeebe985eb28ecefcec0a0f20f9a1
NAGALAKSHMI-CODDER-UX <nagalakshmidongari123@gmail.com>
2026-01-02 18:24:00 +0530

    fgb

--- ConsoleApp59/Program.cs
+++ ConsoleApp59/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp59
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.ReadLine();
+            //Console.ReadLine();
         }
     }
 }
